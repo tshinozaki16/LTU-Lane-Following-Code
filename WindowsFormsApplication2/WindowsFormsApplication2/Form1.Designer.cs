fc1237f0afb7136d3db781f6ca9992f809e76eac
tshinozaki16 <38869532+tshinozaki16@users.noreply.github.com>
2018-04-30 22:55:09 -0400

    Add files via upload

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(522, 416);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(390, 438);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(520, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(473, 416);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(872, 438);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 3;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(154, 438);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(272, 438);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 472);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Threading;
+using System.IO.Ports;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture _capture;
+        private Thread _captureThread;
+        int thresh;
+        int threshred;
+        int balance = 0;
+        int red = 0;
+        int leftside = 0;
+        int rightside = 0;
+        Mat frame;
+        SerialPort _serialPort;
+        byte[] buffer = {0x01, 0, 0};
+        delegate void SetTextCallback(string text1, string text2);
+        public Form1()
+        {
+            InitializeComponent();
+            numericUpDown1.Value = 150;
+            numericUpDown2.Value = 130;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _capture = new VideoCapture();
+            _captureThread = new Thread(DisplayWebcam);
+            _captureThread.Start();
+            _serialPort = new SerialPort("COM4", 2400);
+            _serialPort.DataBits = 8;
+            _serialPort.Parity = Parity.None;
+            _serialPort.StopBits = StopBits.Two;
+            _serialPort.Open();
+        }
+
+        private void SetText(string text1, string text2)
+        {
+            if (this.textBox1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text1, text2 });
+            }
+            else
+            {
+                this.textBox1.Text = text1;
+                this.textBox2.Text = text2;
+            }
+        }
+
+        private void DisplayWebcam()
+        {
+            while (_capture.IsOpened)
+            {
+                frame = _capture.QueryFrame();
+                CvInvoke.Resize(frame, frame, pictureBox1.Size);
+                Image<Gray, Byte> processCapture = frame.ToImage<Gray, Byte>();
+                Image<Hsv, Byte> redProcessCapture = frame.ToImage<Hsv, Byte>();
+                processCapture._ThresholdBinary(new Gray(thresh), new Gray(255));
+                redProcessCapture._ThresholdBinary(new Hsv(200,threshred,150),new Hsv(255,255,255));
+                pictureBox1.Image = processCapture.Bitmap;
+                pictureBox2.Image = redProcessCapture.Bitmap;
+                leftside = 0;
+                rightside = 0;
+                balance = 0;
+                red = 0;
+
+                for (int x = 0; x < processCapture.Width / 2; x++)
+                {
+                    for (int y = 0; y < processCapture.Height; y++)
+                    {
+                        if (processCapture.Data[y, x, 0] == 255)
+                            leftside++;
+                    }
+                }
+
+                for (int x = processCapture.Width/2; x < processCapture.Width; x++)
+                {
+                    for (int y = 0; y < processCapture.Height; y++)
+                    {
+                        if (processCapture.Data[y, x, 0] == 255)
+                            rightside++;
+                    }
+                }
+
+                for(int x = 0; x < processCapture.Width; x++)
+                {
+                    for(int y = 0; y < processCapture.Height; y++)
+                    {
+                        if (processCapture.Data[y, x, 0] == 255)
+                            balance += (x - processCapture.Width / 2);
+                        if ((redProcessCapture.Data[y, x, 2] == 255))
+                            red++;
+                    }
+                }
+                balance /= 10000;
+
+                if (balance < -40)
+                {
+                    buffer[1] = 0x0F;
+                    buffer[2] = 0x6F;
+                }
+                else if(balance > 40)
+                {
+                    buffer[1] = 0x6F;
+                    buffer[2] = 0x0F;
+                }
+                else
+                {
+                    buffer[1] = 0x6F;
+                    buffer[2] = 0x6F;
+                }
+                if (red > 20000)
+                {
+                    this.SetText(balance.ToString(), red.ToString());
+                    Thread.Sleep(1200);
+                    buffer[1] = 0x6F;
+                    buffer[2] = 0x5F;
+                }
+                else
+                {
+                    this.SetText(balance.ToString(), red.ToString());
+                }
+                _serialPort.Write(buffer, 0 ,3);
+            }
+        }
+
+        private void numUpDown1_val(object sender, EventArgs e)
+        {
+            thresh = (byte)numericUpDown1.Value;
+        }
+
+        private void numeUpDown2_val(object sender, EventArgs e)
+        {
+            threshred = (byte)numericUpDown2.Value;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //Ends the program when the camera window is closed.
+            _captureThread.Abort();
+        }
+    }
+}
